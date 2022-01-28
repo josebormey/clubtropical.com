7180ea730e06c17062036ff1ab325d663f77829b
josebormey <josebormey@gmail.com>
2022-01-28 16:10:29 -0600

    tercera actualizacion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -158,7 +158,7 @@
         <!-- Welcome Section Start -->
         <div id="welcome">
             <div class="container">
-                <h3>Bienvenidos a Club Tropical1</h3>
+                <h3>Bienvenidos a Club Tropical</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas gravida sollicitudin turpis id posuere. Fusce nec rhoncus nibh. Fusce arcu libero, euismod eget commodo at, venenatis a nisi. Sed faucibus metus sed leo vulputate blandit.</p>
                 <a href="#">Reservar ahora</a>
             </div>
